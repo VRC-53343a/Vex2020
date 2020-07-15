1232c75cece1f1c45e3dc283a93339a54bf74229
adx59 <adamxu59@gmail.com>
2020-07-15 12:22:12 -0700

    delete useless docs that came with comp template

--- DriverControl/src/main.cpp
+++ DriverControl/src/main.cpp
@@ -79,38 +79,17 @@
 
 using namespace vex;
 
-// A global instance of competition
 competition Competition;
 
 // define your global instances of motors and other devices here
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+
 
 void autonomous(void) {
   // ..........................................................................
@@ -118,32 +97,13 @@ void autonomous(void) {
   // ..........................................................................
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
-  // User control code here, inside the loop
   std::map <std::pair<int, int>, float> quadrant; 
   quadrant[{1, 1}] = 1; quadrant[{0, 1}] = 2; quadrant[{0, 0}] = 3; quadrant[{1, 0}] = 4; 
+  
   while (true) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    wait(20, msec);
 
     std::pair<float, float> strafeAxes (Controller1.Axis4.position(percent),
                                         Controller1.Axis3.position(percent));
@@ -151,9 +111,11 @@ void usercontrol(void) {
 
     float strafeHeading;
     if (strafeQuadrant == 1 || strafeQuadrant == 3) {
-      strafeHeading = fabs(atan(strafeAxes.second / strafeAxes.first)) + ((strafeQuadrant-1)*(PI/2));
+      strafeHeading = fabs(atan(strafeAxes.second / strafeAxes.first))
+                        + ((strafeQuadrant-1)*(PI/2));
     } else {
-      strafeHeading = fabs(atan(strafeAxes.first / strafeAxes.second)) + ((strafeQuadrant-1)*(PI/2));
+      strafeHeading = fabs(atan(strafeAxes.first / strafeAxes.second)) 
+                        + ((strafeQuadrant-1)*(PI/2));
     }
 
     float strafeMagnitude = sqrt(pow(strafeAxes.first, 2) + pow(strafeAxes.second, 2));
