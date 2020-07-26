9c9a1ed929dd50c7b47b47474a35bc8017874953
ShafinH <65793752+ShafinH@users.noreply.github.com>
2020-07-26 11:28:01 -0700

    Completely updated code. (god movement)
    Also has all the motor functions for the buttons.

--- DriverControl/src/main.cpp
+++ DriverControl/src/main.cpp
@@ -1,17 +1,17 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// topLeft              motor         1
-// bottomLeft           motor         2
-// topRight             motor         3
-// bottomRight          motor         4
-// intakeLeft           motor         5
-// intakeRight          motor         6
-// intakeRoller         motor         7
-// flywheel             motor         8
-// Controller1          controller
+// topLeft              motor         1               
+// bottomLeft           motor         2               
+// topRight             motor         3               
+// bottomRight          motor         4               
+// intakeLeft           motor         5               
+// intakeRight          motor         6               
+// intakeRoller         motor         7               
+// flywheel             motor         8               
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
-/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------w-----------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
 /*    Author:       VEX                                                       */
@@ -33,32 +33,7 @@ competition Competition;
 
 std::map <std::pair<int, int>, float> quadrant; 
 
-float * strafeMotorValues (std::pair<float, float> axes) {
-  // TODO: justify max motor values to 100
-  // use static multiplier or some fancy algebra idgaf
 
-  int strafeQuadrant = quadrant[{axes.first < 0 ? 0: 1, axes.second < 0 ? 0: 1}];
-
-  float strafeHeading;
-  if (strafeQuadrant == 1 || strafeQuadrant == 3) {
-    strafeHeading = fabs(atan(axes.second / axes.first))
-                      + ((strafeQuadrant-1)*(PI/2));
-  } else {
-    strafeHeading = fabs(atan(axes.first / axes.second)) 
-                      + ((strafeQuadrant-1)*(PI/2));
-  }
-
-  float strafeMagnitude = sqrt(pow(axes.first, 2) + pow(axes.second, 2));
-
-  float * motorValues = new float[4] {
-    static_cast<float>(round(strafeMagnitude * cos((3 * PI / 4) + strafeHeading))), // front left
-    static_cast<float>(round(strafeMagnitude * cos ((3 * PI / 4) - strafeHeading))), // front right
-    static_cast<float>(round((-1 * strafeMagnitude) * cos((3 * PI / 4) - strafeHeading))), // back left 
-    static_cast<float>(round((-1 * strafeMagnitude) * cos ((3 * PI / 4) + strafeHeading))) // back right
-  };
-
-  return motorValues;
-}
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
@@ -79,27 +54,37 @@ void usercontrol(void) {
   int flywheelSpeed = 100;
   while (true) {
     wait(20, msec);
+    int ax1 = Controller1.Axis1.value();
+    int ax2 = Controller1.Axis2.value();
+    int ax3 = Controller1.Axis3.value();
+    int ax4 = Controller1.Axis4.value();
 
-    std::pair<float, float> strafeAxes (Controller1.Axis4.position(percent),
-                                        Controller1.Axis3.position(percent));
 
-    float * motorVals = strafeMotorValues(strafeAxes);
+    topRight.spin(directionType::fwd, ax2-ax4-ax1, velocityUnits::pct);
+    bottomRight.spin(directionType::fwd, ax2-ax4+ax1, velocityUnits::pct);
+    topLeft.spin(directionType::fwd, ax2+ax4+ax1, velocityUnits::pct);
+    bottomLeft.spin(directionType::fwd, ax2+ax4-ax1, velocityUnits::pct);
 
-    topLeft.spin(directionType::fwd, *(motorVals), velocityUnits::pct);
-    topRight.spin(directionType::fwd, *(motorVals + 1), velocityUnits::pct);
-    bottomLeft.spin(directionType::fwd, *(motorVals + 2), velocityUnits::pct);
-    bottomRight.spin(directionType::fwd, *(motorVals + 3), velocityUnits::pct);
+    topRight.spin(directionType::fwd, ax3-ax1-ax4, velocityUnits::pct);
+    bottomRight.spin(directionType::fwd, ax3-ax1+ax4, velocityUnits::pct);
+    topLeft.spin(directionType::fwd, ax3+ax1+ax4, velocityUnits::pct);
+    bottomLeft.spin(directionType::fwd, ax3+ax1-ax4, velocityUnits::pct);
+
+
 
     if(Controller1.ButtonR2.pressing()) {
             intakeLeft.spin(directionType::rev, intakeSpeed, velocityUnits::pct);
             intakeRight.spin(directionType::fwd, intakeSpeed, velocityUnits::pct);
             intakeRoller.spin(directionType::fwd, intakeSpeed, velocityUnits::pct);
         }
-    else if(Controller1.ButtonDown.pressing()) {
+    else if(Controller1.ButtonL2.pressing()) {
             intakeLeft.spin(directionType::fwd, intakeSpeed, velocityUnits::pct);
             intakeRight.spin(directionType::rev, intakeSpeed, velocityUnits::pct);
             intakeRoller.spin(directionType::rev, intakeSpeed, velocityUnits::pct);
         }
+    else if(Controller1.ButtonL1.pressing()){
+            intakeRoller.spin(directionType::fwd, intakeSpeed, velocityUnits::pct);
+    }
     else {
             intakeLeft.stop(brakeType::brake);
             intakeRight.stop(brakeType::brake);
@@ -112,6 +97,8 @@ void usercontrol(void) {
     else {
             flywheel.stop(brakeType::brake);        
         }
+
+   
 
   }
 }
